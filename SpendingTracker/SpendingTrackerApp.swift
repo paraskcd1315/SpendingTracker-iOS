ada73692ad708d0788172475febffb94bacf71a8
Paras Khanchandani <dev@paraskcd.com>
2024-05-11 13:08:38 +0200

    Initial Commit

---
+++ SpendingTracker/SpendingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingTrackerApp.swift
+//  SpendingTracker
+//
+//  Created by Paras KCD on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
